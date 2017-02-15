8fd6a785b99d30b890f4d21192d3da4caaaaad26
Pia Bregulla <pia.bregulla@xing.com>
2017-02-15 11:17:44 +0100

    try to random pick quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,11 @@
 class QuotesController < ApplicationController
   def index
-    @quotes = Quote.all
+    @quote = Quote.order("RANDOM()").first
+  #  @quotes = Quote.all
+  #hat alle quotes gezeigt, aber nicht die abfrage box
   end
+
+  def new
+  end
+  #reserves a column for new quotes in our database
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root'quotes#index'
+
+  resources :quotes
+  #The priority is based upon order of creation: first created -> highest priority
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
