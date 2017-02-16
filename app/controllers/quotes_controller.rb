90c251acc68871fd43392c2ae248b4c7b0f5677f
Pia Bregulla <pia.bregulla@xing.com>
2017-02-16 11:38:08 +0100

    quote injection and redirection

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,4 +9,15 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
   #reserves a column for new quotes in our database
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
