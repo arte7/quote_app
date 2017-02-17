1a0d43986ccd97051fd6b84c2cbf696eb4d5693d
Pia Bregulla <pia.bregulla@xing.com>
2017-02-17 13:50:21 +0100

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root'quotes#index'
-
   resources :quotes
+  get 'about', to: 'quotes#about'
   #The priority is based upon order of creation: first created -> highest priority
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
